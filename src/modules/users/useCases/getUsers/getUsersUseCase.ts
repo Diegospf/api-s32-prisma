455f61914932e37b7e4f9380a62413b4b3670d34
Diego Pires <diegospf13@gmail.com>
2024-01-24 13:58:05 -0300

    fix

--- src/modules/users/useCases/getUsers/getUsersUseCase.ts
+++ src/modules/users/useCases/getUsers/getUsersUseCase.ts
@@ -1,13 +1,11 @@
-import { User } from "@prisma/client";
 import { prisma } from "../../../../prisma/client";
 
 export class UserUseCase {
   async getAllUsers(): Promise<{ id: string; }[]> {
     try {
-      const users: User[] = await prisma.user.findMany({
+      const users = await prisma.user.findMany({
         select: {
           username: true,
-          password: true,
         },
       });
 
